ff53da9b66c335093f68e5058167fd1c3d1742ee
jzilic1991 <jzilic91@gmail.com>
2021-11-25 18:00:00 +0100

    Fetch HTTP response from Kubernetes cluster

--- mobile_device/src/main.py
+++ mobile_device/src/main.py
@@ -1,15 +1,32 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty, ObjectProperty
+from kivy.network.urlrequest import UrlRequest
+from kivy.lang import Builder
 
-class MainApp(App):
+class MyWidget(BoxLayout):
+   
+    label_text = StringProperty()
+
+    def __init__ (self, **kwargs):
+        super (MyWidget, self).__init__(**kwargs)
+        
+        self.label_text = "Waiting for HTTP response..."
+        self._search_url = "http://128.131.169.143:30927"
+        self._request = UrlRequest (self._search_url, self.http_response)
+
+
+    def http_response (self, *args):
+        self.label_text = self._request.result
+
+
+class MyApp(App):
+
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+        return MyWidget()
 
-        return label
+
+Builder.load_file("main_layout.kv")
 
 if __name__ == '__main__':
-    app = MainApp()
-    app.run()
-
+    MyApp().run()
