024adfcc916bc751cd5255d2fdf7824b558992e6
jzilic1991 <jzilic91@gmail.com>
2021-12-16 23:17:55 +0100

    Fix Dockerfile for failure monitor

--- offloading_site/prediction_engine/app/main.py
+++ offloading_site/prediction_engine/app/main.py
@@ -1,8 +1,5 @@
-import warnings
-
 from socket_server import SocketServer
 
 
 if __name__ == "__main__":
-    warnings.filterwarnings ('ignore', category = DeprecationWarning)
     SocketServer('localhost', 8001).run()
