bdaae5962d93f8de2be2e1ff70a489ea6c05e986
jzilic1991 <jzilic91@gmail.com>
2021-12-16 20:03:46 +0100

    Fixing Docker and requirements files

--- offloading_site/prediction_engine/app/main.py
+++ offloading_site/prediction_engine/app/main.py
@@ -1,5 +1,8 @@
+import warnings
+
 from socket_server import SocketServer
 
 
 if __name__ == "__main__":
+    warnings.filterwarnings ('ignore', category = DeprecationWarning)
     SocketServer('localhost', 8001).run()
